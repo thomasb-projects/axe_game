38fb81b1aeda7d75b6e4e191f0d5553151fa1c40
thomas.b <thomasb.business@protonmail.com>
2022-06-16 22:02:30 -0500

    Change axe_game.cpp to reflect new understanding of moving drawn objects via timed updates and key inputs

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,15 +1,35 @@
 #include "raylib.h"
 int main()
 {
-    int width;
-    width = 350;
-    InitWindow(width,200,"Window of AxeGame");
+    int width(350);
+    int height(200);
+    InitWindow(width,height,"Window of AxeGame");
 
+    int circle_x{175};
+    int circle_y{100};
+
+
+    SetTargetFPS(60);
     while (WindowShouldClose() != true)
     {
         BeginDrawing();
-        ClearBackground(RED);
-        DrawCircle(175, 100, 25, BLUE);
+        ClearBackground(WHITE);
+
+        //game logic begins
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        //game logic ends
         EndDrawing();
     }
 }
