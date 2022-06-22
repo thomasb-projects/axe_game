74f6d5d8ffbe3da9f89223138d9343f6037a4ad4
thomas.b <thomasb.business@protonmail.com>
2022-06-22 10:19:57 -0500

    Update axe_game.cpp with variables for use in hit detection

--- axe_game.cpp
+++ axe_game.cpp
@@ -10,9 +10,28 @@ int main()
     int circle_x{200};
     int circle_y{200};
 
+    //circle radius
+    int circle_radius{25};
+
+    //axe dimensions
+    int axe_width{50};
+    int axe_height{50};
+
     //axe coordinates
     int axe_x{400};
     int axe_y{0};
+
+    //circle hitbox coordinates
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
+    //axe hitbox coordinates
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_width};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_height};
 
     //set axe direction
     int direction{10};
@@ -25,7 +44,7 @@ int main()
 
         //game logic begins
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
         DrawRectangle(axe_x,axe_y,50,50,RED);
         
         //move the axe
